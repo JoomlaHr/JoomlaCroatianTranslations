e3175625bce6daca6c49ecb4a232c76dafc44106
wooer <wooer@92b72da1-4a73-4996-82eb-3326536ea0cc>
2012-06-14 15:27:41 +0000

    Prijevod TinyMCE 3.5.1.1

---
+++ trunk/2.5/hr-HR_TinyMCE_3.5.1.1/tinymce/jscripts/tiny_mce/plugins/autosave/langs/cro.js
@@ -0,0 +1,4 @@
+tinyMCE.addI18n('cro.autosave',{
+restore_content: "Vra\u0107anje automatski spremljenog sadržaja",
+warning_message: "Ako vratite spremljeni sadr\u017eaj, izgubit \u0107ete sav sadr\u017eaj koja je trenutno u editoru.\n\nJeste li sigurni da \u017eelite vratiti spremljeni sadr\u017eaj?"
+});
